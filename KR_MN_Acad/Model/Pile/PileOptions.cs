6a7be6620bdfebc9aff20e301780148065d9098d
Vildar Khisyametdinov <vildar82@gmail.com>
2016-05-26 17:32:26 +0300

    pile options

--- KR_MN_Acad/Model/Pile/PileOptions.cs
+++ KR_MN_Acad/Model/Pile/PileOptions.cs
@@ -18,49 +18,53 @@ namespace KR_MN_Acad.Model.Pile
         public static string FileXml = Path.Combine(AutoCAD_PIK_Manager.Settings.PikSettings.ServerShareSettingsFolder, @"КР-МН\Сваи\PileOptions.xml");
         public const string DictNod = "PIK";
         public const string RecAbsoluteZero = "AbsoluteZero";
+        public const string RecDimPileBeatToCut = "Срубка";
+        public const string RecDimPileCutToRostwerk = "Заделка";
 
         [Category("Общие")]
         [DisplayName("Имя блока сваи")]
         [Description("^КР_свая - имя блока начинается с КР_свая. Регистр игнорируется.")]
-        //[DefaultValue("^КР_свая")]
-        public string PileBlockNameMatch { get; set; }
+        [DefaultValue("^КР_свая")]
+        public string PileBlockNameMatch { get; set; } = "^КР_свая";
 
         [Category("Общие")]
         [DisplayName("Атрибут номера")]
         [Description("Тэг атрибута номера сваи.")]
-        //[DefaultValue("ПОЗ")]
-        public string PileAttrPos { get; set; }
+        [DefaultValue("ПОЗ")]
+        public string PileAttrPos { get; set; } = "ПОЗ";
 
         [Category("Общие")]
         [DisplayName("Минимальное расстояние")]
         [Description("Коэфициент минимального расстояния между сваями. Lmin = k * 'сторона сваи'.")]
-        //[DefaultValue(3)]
-        public double PileRatioLmin { get; set; }        
+        [DefaultValue(3)]
+        public double PileRatioLmin { get; set; } = 3;
 
+        [XmlIgnore]
         [Category("Ростверк")]
         [DisplayName("Срубка, мм")]
-        [Description("Расстояние от верха сваи до срубки, мм.")]
-        //[DefaultValue(300)]
-        public double DimPileBeatToCut { get; set; }
+        [Description("Расстояние от верха сваи до срубки, мм. \nХранится в чертеже.")]
+        [DefaultValue(250)]
+        public double DimPileBeatToCut { get; set; } = 250;
 
+        [XmlIgnore]
         [Category("Ростверк")]
         [DisplayName("Заделка, мм")]
-        [Description("Расстояние от низа ростверка до верха сваи после срубки, мм.")]
-        //[DefaultValue(50)]
-        public double DimPileCutToRostwerk { get; set; }
+        [Description("Расстояние от низа ростверка до верха сваи после срубки, мм. \nДля шарнирного узла заделки сваи - 50мм, для жесткого - 100мм. \nХранится в чертеже.")]
+        [DefaultValue(50)]
+        public double DimPileCutToRostwerk { get; set; } = 50;
 
         [XmlIgnore]
         [Category("Ростверк")]
         [DisplayName("Абс.отметка")]
-        [Description("Абсолютная отметка 0. Например 141.700")]
-        //[DefaultValue(50)]
-        public double AbsoluteZero { get; set; }
+        [Description("Абсолютная отметка 0. Например 141.700. \nХранится в чертеже.")]
+        [DefaultValue(150.00)]
+        public double AbsoluteZero { get; set; } = 150.00;
 
         [Category("Разное")]
         [DisplayName("Слой таблиц")]
         [Description("Слой для вставки таблиц свай.")]
-        //[DefaultValue("КР_Таблицы")]
-        public string TableLayer { get; set; }        
+        [DefaultValue("КР_Таблицы")]
+        public string TableLayer { get; set; } = "КР_Таблицы";
 
         public PileOptions PromptOptions()
         {
@@ -93,21 +97,17 @@ namespace KR_MN_Acad.Model.Pile
                 try
                 {
                     // Загрузка настроек таблицы из файла XML
-                    options = PileOptions.LoadFromXml();
-                    // Загрузка начтроек чертежа
-                    options.LoadFromNOD();
+                    options = PileOptions.LoadFromXml();                    
                 }
                 catch (Exception ex)
                 {
                     Logger.Log.Error(ex, $"Ошибка при попытке загрузки настроек таблицы из XML файла {FileXml}");
                 }
             }
-
             if (options == null)
             {
                 // Создать дефолтные
-                options = new PileOptions();
-                options.SetDefault();
+                options = new PileOptions();                
                 // Сохранение дефолтных настроек 
                 try
                 {
@@ -118,18 +118,11 @@ namespace KR_MN_Acad.Model.Pile
                     Logger.Log.Error(exSave, $"Попытка сохранение настроек в файл {FileXml}");
                 }
             }
-            return options;
-        }       
+            // Загрузка начтроек чертежа
+            options.LoadFromNOD();
 
-        private void SetDefault()
-        {
-            PileBlockNameMatch= "^КР_свая";
-            PileAttrPos = "ПОЗ";
-            PileRatioLmin = 3;
-            DimPileBeatToCut = 300;
-            DimPileCutToRostwerk = 50;
-            TableLayer = "КР_Таблицы";            
-        }
+            return options;
+        }               
 
         private static PileOptions LoadFromXml()
         {
@@ -148,12 +141,16 @@ namespace KR_MN_Acad.Model.Pile
         {
             var nod = new AcadLib.DictNOD(DictNod);
             nod.Save(AbsoluteZero, RecAbsoluteZero);
+            nod.Save(DimPileBeatToCut, RecDimPileBeatToCut);
+            nod.Save(DimPileCutToRostwerk, RecDimPileCutToRostwerk);
         }
 
         private void LoadFromNOD()
         {
             var nod = new AcadLib.DictNOD(DictNod);
-            AbsoluteZero = nod.Load(RecAbsoluteZero, 150.00);
+            AbsoluteZero = nod.Load(RecAbsoluteZero, AbsoluteZero);
+            DimPileBeatToCut = nod.Load(RecDimPileBeatToCut, DimPileBeatToCut);
+            DimPileCutToRostwerk = nod.Load(RecDimPileCutToRostwerk, DimPileCutToRostwerk);
         }
     }
 }
