610349ff9bfa28f1702e03900fb0fc0371afd5b1
Vildar Khisyametdinov <vildar82@gmail.com>
2016-08-17 12:42:06 +0300

    fix getWidthVerticArm - если меньше нуля то =0. Чтобы получилась 1 стернень хотябы

--- KR_MN_Acad/Model/Spec/ArmWall/Blocks/WallBase.cs
+++ KR_MN_Acad/Model/Spec/ArmWall/Blocks/WallBase.cs
@@ -82,8 +82,10 @@ namespace KR_MN_Acad.Spec.ArmWall.Blocks
 		/// </summary>        
 		protected int getWidthVerticArm (int step, int length)
 		{
-			// Вычесть отступ у торцов стены = шаг вертик стержней - а.            
-			return length - (step - a) * 2;
+			// Вычесть отступ у торцов стены = шаг вертик стержней - а.  
+            var res = length - (step - a) * 2;
+            if (res < 0) res = 0;
+            return res;
 		}
 
 		/// <summary>
